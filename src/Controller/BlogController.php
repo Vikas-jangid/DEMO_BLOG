24ae8bc164efc44465945b3a45beeccabbc1217f
Vikas-jangid <vikas.jangid@infobeans.com>
2021-08-05 15:26:53 +0530

    add blog form added

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,20 +1,29 @@
 <?php
 
 namespace App\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 //use  Symfony\Component\HttpFoundation\Response;
 
-use Symfony\Component\Routing\Annotations\Route;
 
 class BlogController extends AbstractController
 {
-    public function index(){
-        
         /**
-         * @Route("/");
+         * @Route("/" , name="home")
          */
+    public function home(){
+        
     return $this->render('blog_view/AddBlog.html.twig');
+    }
+    
+    /**
+     * @Route("/blog", name="blogs")
+     */
+    
+    public function blog(){
+        
+    return $this->render('blog_view/showBlog.html.twig');
     }
 }
