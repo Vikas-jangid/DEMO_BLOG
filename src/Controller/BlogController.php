0d9ab320cf985769380ee5bcbe9c6eab7bcfe57c
Vikas-jangid <vikas.jangid@infobeans.com>
2021-08-16 12:29:29 +0530

    Blog view changed from card to table

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,29 +1,65 @@
 <?php
 
 namespace App\Controller;
-use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Blog;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-//use  Symfony\Component\HttpFoundation\Response;
+class BlogController extends AbstractController {
+
+/**
+ * @Route("/" , name="home")
+ * @Method({"POST"})
+ */
+public function home() {
+
+return $this->render('blog_view/AddBlog.html.twig');
+}
 
 
-class BlogController extends AbstractController
-{
-        /**
-         * @Route("/" , name="home")
-         */
-    public function home(){
-        
-    return $this->render('blog_view/AddBlog.html.twig');
-    }
-    
-    /**
-     * @Route("/blog", name="blogs")
-     */
-    
-    public function blog(){
-        
-    return $this->render('blog_view/showBlog.html.twig');
-    }
+
+/**
+ * @Route("/blog", name="blogs")
+ * 
+ * @Method({"GET"})
+ */
+public function blog() {
+
+
+$blogs = $this->getDoctrine()->getRepository(Blog::class)->findAll();
+
+//        dump($blogs);
+//        
+//        exit;
+
+return $this->render('blog_view/showBlog.html.twig', array('blogs' => $blogs));
+
+
+}
+
+
+
+
+/**
+ * @Route("/blog/save")
+ */
+public function BlogSave() {
+
+//        $entityManager = $this->getDoctrine()->getManager();
+//        
+//        $Blog = new Blog();
+//        
+//        $Blog->setBlog_Title('BLOG TITLE ONE');
+//        $Blog->setAuthor('Author One');
+//        $Blog->setBlog_description('BLOG description is here');
+//        
+//        $entityManager->persist($Blog);
+//        
+//        $entityManager->flush();
+//
+//        return new Response('saves an blog with the end of ' . $Blog->getId());
+}
+
 }
